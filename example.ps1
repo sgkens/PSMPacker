0da3841117a89106173035ccd21b6d88c2b11091
snoonx <snoonx@snowlab.tk>
2023-04-25 13:55:15 +1000

    deinit submodules

--- example.ps1
+++ example.ps1
@@ -1,1 +1,8 @@
-Copy-BuildToVersionFolder -SourcePath G:\devspace\projects\powershell\quicklog -DestinationPath G:\devspace\projects\powershell\quicklog\dist -FilesToCopy @("quicklog.ps1") -FoldersToCopy @("libs") -IncrementVersion "build" -apira
+using module libs\Copy-BuildToVersionFolder.psm1
+Copy-BuildToVersionFolder -SourcePath G:\devspace\projects\powershell\_repos\BuildTVF `
+                          -DestinationPath G:\devspace\projects\powershell\_repos\BuildTVF\dist`
+                          -FilesToCopy @("buildTVF.psm1","buildTVF.psd1","build.ps1","readme.md","license")`
+                          -FoldersToCopy @("libs")`
+                          -IncrementVersion "build"`
+                          -name "BuildTVF"
+                          #-apira
