acd9f97d960b1eb04b2924abc9376d3ace192bef
sgkens <sgkens@gmail.com>
2025-01-26 00:23:48 +1100

    👷 cicd(build script): UPDATE: Self Build

--- build/bt1-ci-build-module.ps1
+++ build/bt1-ci-build-module.ps1
@@ -1,13 +1,15 @@
 using module ..\libs\Build-Module.psm1
 
-Remove-Item -Path .\dist\PSMPacker -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path .\dist\psmpacker -Recurse -Force -ErrorAction SilentlyContinue
+
 $AutoVersion = (Get-GitAutoVersion).Version
 
 Build-Module -SourcePath .\ `
              -DestinationPath .\dist `
-             -Name "PSMPacker" `
+             -Name "psmpacker" `
              -IncrementVersion None `
-             -FilesToCopy "PSMPacker.psd1","license","PSMPacker.psm1","icon.png" `
+             -FilesToCopy "psmpacker.psd1","license","psmpacker.psm1","icon.png" `
              -FoldersToCopy "libs" `
              -Manifest `
+             -Dependencies @(@{type="module";name="quicklog";version="1.2.3"}) `
              -Version $AutoVersion
